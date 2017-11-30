556e6c655a9fa1b7faae5aa56c1f6dee4edadadd
Vildan Tursic <vildantursic@hotmail.com>
2017-11-30 23:29:51 +0100

    css grid implementation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   articles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
   }
 
 }
