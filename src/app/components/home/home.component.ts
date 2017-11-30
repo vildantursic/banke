7b09b824a3153b44f6da8798c7b84dc740571e3a
Vildan Tursic <vildantursic@hotmail.com>
2017-12-01 00:30:36 +0100

    fixed css grid layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,7 @@ import {Component, AfterViewInit} from '@angular/core';
 export class HomeComponent implements AfterViewInit {
 
   articles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  articlesSmall = [1, 2, 3, 4, 5]
 
   constructor() { }
 
