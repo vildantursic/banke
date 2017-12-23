4012137ed935cea291cdbf577ca957f1fe052280
Vildan Tursic <vildantursic@hotmail.com>
2017-12-24 00:18:26 +0100

    minor style fixes and updates, share buttons implementation

--- src/app/shared/social/social.component.ts
+++ src/app/shared/social/social.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-social',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social.component.scss']
 })
 export class SocialComponent implements OnInit {
+
+  @Input() slug;
 
   constructor() { }
 
